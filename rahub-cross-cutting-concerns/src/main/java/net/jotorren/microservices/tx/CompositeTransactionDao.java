731e860d107b1068568aafda82bc0d7179e9323b
Jorge Torrente <jorge.torrente@t-systems.com>
2017-05-10 18:49:16 +0200

    Unsynchronized DAO implementation for inserts/updates/deletes

---
+++ rahub-cross-cutting-concerns/src/main/java/net/jotorren/microservices/tx/CompositeTransactionDao.java
@@ -0,0 +1,50 @@
+package net.jotorren.microservices.tx;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.SynchronizationType;
+
+public class CompositeTransactionDao {
+
+	@PersistenceContext(type = PersistenceContextType.EXTENDED, synchronization = SynchronizationType.UNSYNCHRONIZED)
+	protected EntityManager em;
+
+	public void save(Object entity) {
+		em.persist(entity);
+	}
+
+	public <T> T saveOrUpdate(T entity) {
+		return em.merge(entity);
+	}
+
+	public void remove(Object entity) {
+		em.remove(entity);
+	}
+
+	public void commit() {
+		em.joinTransaction();
+	}
+
+	public void apply(List<EntityCommand<?>> transactionOperations) {
+		if (null == transactionOperations) {
+			return;
+		}
+
+		for (EntityCommand<?> command : transactionOperations) {
+			switch (command.getAction().ordinal()) {
+			case 0:
+				save(command.getEntity());
+				break;
+			case 1:
+				saveOrUpdate(command.getEntity());
+				break;
+			case 2:
+				remove(command.getEntity());
+				break;
+			}
+		}
+	}
+}
