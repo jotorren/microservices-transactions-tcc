180c7f3d075ad863a18e75570b95996de59edf32
Jorge Torrente <jorge.torrente@t-systems.com>
2017-05-11 18:56:58 +0200

    swagger 2 added to composite service

--- rahub-composite-service/src/main/java/net/jotorren/microservices/composite/controller/CompositeController.java
+++ rahub-composite-service/src/main/java/net/jotorren/microservices/composite/controller/CompositeController.java
@@ -1,5 +1,11 @@
 package net.jotorren.microservices.composite.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import java.net.URI;
 import java.util.List;
 import java.util.Map.Entry;
@@ -13,6 +19,7 @@ import javax.ws.rs.core.UriInfo;
 
 import net.jotorren.microservices.composite.domain.CompositeData;
 import net.jotorren.microservices.composite.service.CompositeService;
+import net.jotorren.microservices.rs.ExceptionRestHandler.ErrorDetails;
 import net.jotorren.microservices.tx.CompositeTransactionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +27,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 @Path("/api")
+@Api(value = "/api")
 public class CompositeController {
 
 	@Autowired
@@ -28,7 +36,21 @@ public class CompositeController {
 	@POST
 	@Path("/files")
 	@Produces ( "application/json" )
-	public Response save(@Context UriInfo uriInfo, CompositeData data) throws CompositeTransactionException {
+    @ApiOperation(
+    		code = 201,
+            value = "Save new content while creating its discussion board",
+            notes = "Both operations (new content and forum) must succeed within a given time interval, otherwise both will be canceled or rolled back. "
+            		+ "The newly created resource(s) can be referenced by the URI(s) returned in the entity of the response, with the URI for the "
+            		+ "distributed transaction given by the Location header field",
+            response = String.class,
+            responseContainer = "List"
+        )
+	@ApiResponses(value = {
+			@ApiResponse(code=503, message="Internal error", response = ErrorDetails.class)
+	})
+	public Response save(@Context UriInfo uriInfo, 
+			@ApiParam(value = "Data to pass to server", required = true) CompositeData data
+			) throws CompositeTransactionException {
 		
 		Entry<String, List<String>> txEntities = service.saveAllEntities(data);
 		
