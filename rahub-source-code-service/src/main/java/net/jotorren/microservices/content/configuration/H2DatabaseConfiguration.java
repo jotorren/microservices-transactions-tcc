793727c7e066df43db9fed3cda26ea0a5f38595e
Jorge Torrente <jorge.torrente@t-systems.com>
2017-05-11 13:44:39 +0200

    H2 web console URL changed to "/h2/console"

--- rahub-source-code-service/src/main/java/net/jotorren/microservices/content/configuration/H2DatabaseConfiguration.java
+++ rahub-source-code-service/src/main/java/net/jotorren/microservices/content/configuration/H2DatabaseConfiguration.java
@@ -11,7 +11,7 @@ public class H2DatabaseConfiguration {
 	@Bean
 	ServletRegistrationBean h2servletRegistration() {
 		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-		registrationBean.addUrlMappings("/console/*");
+		registrationBean.addUrlMappings("/h2/console/*");
 		return registrationBean;
 	}
 }
