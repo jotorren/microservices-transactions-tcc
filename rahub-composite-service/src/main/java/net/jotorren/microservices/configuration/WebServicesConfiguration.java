7a5ac6c3d7c35a3b775d18b22ec9360dcf31cffa
Jorge Torrente <jorge.torrente@t-systems.com>
2017-05-11 13:42:22 +0200

    RestTemplate definition added

--- rahub-composite-service/src/main/java/net/jotorren/microservices/configuration/WebServicesConfiguration.java
+++ rahub-composite-service/src/main/java/net/jotorren/microservices/configuration/WebServicesConfiguration.java
@@ -11,8 +11,10 @@ import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.feature.LoggingFeature;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import com.atomikos.icatch.tcc.rest.CoordinatorImp;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
@@ -46,4 +48,9 @@ public class WebServicesConfiguration {
         
         return endpoint.create();
     }
+    
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
