4a7a238e1fe098fe4deb4450707a92a167374f9c
Jorge Torrente <jorge.torrente@t-systems.com>
2017-05-17 13:45:16 +0200

    partial commits must control potential errors

--- rahub-cross-cutting-concerns/src/main/java/net/jotorren/microservices/tx/CompositeTransactionParticipantController.java
+++ rahub-cross-cutting-concerns/src/main/java/net/jotorren/microservices/tx/CompositeTransactionParticipantController.java
@@ -2,12 +2,19 @@ package net.jotorren.microservices.tx;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import net.jotorren.microservices.rs.ExceptionRestHandler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +27,8 @@ public abstract class CompositeTransactionParticipantController {
 	@Path("/tcc/{txid}")
 	@Consumes("application/tcc")
     @ApiOperation(
+    		code = 204,
+    		response = String.class,
             value = "Rollback a given composite transaction",
             notes = "See https://www.atomikos.com/Blog/TransactionManagementAPIForRESTTCC",
             consumes = "application/tcc"
@@ -38,18 +47,30 @@ public abstract class CompositeTransactionParticipantController {
 	@Path("/tcc/{txid}")
 	@Consumes("application/tcc")
     @ApiOperation(
+    		code = 204,
+    		response = String.class,
             value = "Commit a given composite transaction",
             notes = "See https://www.atomikos.com/Blog/TransactionManagementAPIForRESTTCC",
             consumes = "application/tcc"
         )
+	@ApiResponses(value = {
+			@ApiResponse(code=404, message="Error committing transaction", response = String.class)
+	})
 	public void confirm(
 			@ApiParam(value = "Id of the composite transaction to commit", required = true) @PathParam("txid") String txid
 		){
 		LOG.info("Trying to commit transaction [{}]", txid);
 		
-		getCompositeTransactionParticipantService().confirm(txid);
+		try {
+			getCompositeTransactionParticipantService().confirm(txid);
 		
-		LOG.info("Transaction [{}] committed", txid);
+			LOG.info("Transaction [{}] committed", txid);
+		} catch(Exception e){
+			// See com.atomikos.icatch.tcc.rest.ParticipantAdapterImp.callConfirmOnJaxrsClient()
+			Response response =	Response.status(Response.Status.NOT_FOUND)
+					.entity(new ExceptionRestHandler().toString(e)).type(MediaType.TEXT_PLAIN).build();
+			throw new WebApplicationException(response);			
+		}
 	}
 	
 	public abstract CompositeTransactionParticipantService getCompositeTransactionParticipantService();
